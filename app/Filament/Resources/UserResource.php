fac07e8145ceb493fe953bce61c36be883a5efff
atenahdv <atenahdv@gmail.com>
2025-07-12 19:48:23 +0200

    complete user

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -3,16 +3,16 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\UserResource\Pages;
-use App\Filament\Resources\UserResource\RelationManagers;
 use App\Models\User;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
-
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
 class UserResource extends Resource
 {
     protected static ?string $model = User::class;
@@ -20,31 +20,62 @@ class UserResource extends Resource
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
     public static function form(Form $form): Form
-    {
-        return $form
-            ->schema([
-                //
-            ]);
-    }
+{
+    return $form
+        ->schema([
+            TextInput::make('name')
+                ->required()
+                ->maxLength(255),
 
-    public static function table(Table $table): Table
-    {
-        return $table
-            ->columns([
-                //
-            ])
-            ->filters([
-                //
-            ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
-            ]);
-    }
+            TextInput::make('email')
+                ->email()
+                ->required()
+                ->maxLength(255),
+
+            TextInput::make('password')
+                ->password()
+                ->required(fn (string $context) => $context === 'create')
+                ->dehydrateStateUsing(fn ($state) => !empty($state) ? bcrypt($state) : null)
+                ->maxLength(255),
+
+            Toggle::make('is_active')
+                ->default(true),
+
+        ]);
+}
+
+public static function table(Table $table): Table
+{
+    return $table
+        ->columns([
+            TextColumn::make('id')
+                ->sortable(),
+
+            TextColumn::make('name')
+                ->searchable()
+                ->sortable(),
+
+            TextColumn::make('email')
+                ->searchable(),
+
+            IconColumn::make('is_active')
+                    ->boolean()
+                    ->label('Activated'),
+
+            TextColumn::make('created_at')
+            ->dateTime('Y-m-d H:i')
+            ->sortable(),
+        ])
+        ->filters([
+            //
+        ])
+        ->actions([
+            Tables\Actions\EditAction::make(),
+        ])
+        ->bulkActions([
+            Tables\Actions\DeleteBulkAction::make(),
+        ]);
+}
 
     public static function getRelations(): array
     {
